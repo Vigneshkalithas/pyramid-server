43c4426c6513ffcdca5fe0dc54dd275047ea4204
vigneshkalithas <vigneshkanna3333@gmail.com>
2023-02-08 23:20:47 +0530

    cors error fixed

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 
-app.use(
-  cors({
-    origin: "https://merry-sunflower-95229b.netlify.app/",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://merry-sunflower-95229b.netlify.app/",
+//   })
+// );
 
 try {
   await mongoose.connect(process.env.MONGO_URL);
